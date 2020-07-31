d60ebf37bc1211dcba3bd5df135c0495246949cf
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-31 16:06:53 -0700

    add city property to client

--- Salon/Models/Client.cs
+++ Salon/Models/Client.cs
@@ -5,6 +5,7 @@ namespace Salon.Models
     public int ClientId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public string City { get; set; } = "Portland";
     public int StylistId { get; set; } = 0;
 
     public virtual Stylist Stylist { get; set; }
