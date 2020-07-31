3c74496406d0a33a3b16a0d34106483b25a3b4d3
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-31 13:40:38 -0700

    add Detail, Create, and Edit features for Client

--- Salon/Controllers/ClientController.cs
+++ Salon/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Salon.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,37 @@ namespace Salon.Controllers
     {
       List<Client> model = _db.Clients.ToList();
       return View(model);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(Client => Client.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
