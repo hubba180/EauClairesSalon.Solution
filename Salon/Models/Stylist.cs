370b8eb292db1ed001fae1c9889e352b61504a25
Kyle Hubbard <kyle.james.hubbard@gmail.com>
2020-07-31 09:11:59 -0700

    add Stylist object

--- Salon/Models/Stylist.cs
+++ Salon/Models/Stylist.cs
@@ -2,6 +2,11 @@ namespace Salon.Models
 {
   public class Stylist
   {
-    
+    public Stylist()
+    {
+      
+    }
+    public string Name { get; set; }
+    public int StylistId { get; set; }
   }
 }
